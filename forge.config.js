e66153560e776672ee038b8d0ffee0def80322af
Dylan <dylan@whitesn.net>
2023-04-20 01:27:09 +0700

    Simplified README

--- forge.config.js
+++ forge.config.js
@@ -19,4 +19,17 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'whitesn',
+          name: 'rew-autoeq-zapco',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
 };
